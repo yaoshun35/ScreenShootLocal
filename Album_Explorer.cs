9a983c7cd74a007298dbc0bf77e7a4ed9363c65f
yaoshun35 <yaoshun35@qq.com>
2019-01-28 18:05:12 +0800

    基础脚本：拍摄/相册翻阅
    拍摄/相册翻阅。 截图到PC的data文件夹，一些元素，如倒计时、翻页声音，可以详见资源包中的示例场景

---
+++ Album_Explorer.cs
@@ -0,0 +1,264 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEngine.UI;
+using System;
+
+
+//1.if too many photos in floder ,we need to clear it 
+//2.OPen and show pictures on the ALbum
+
+public class Album_Explorer : MonoBehaviour {
+
+    //ugui image element
+    public Image _AlbumPage;
+
+    //where the photo saved
+    private string PhotoPath;
+    // turn on/off the album window;
+    private bool AlbumState=false;
+
+    //hide something when album opened
+    public GameObject[] hideBUttons;
+
+    //which photo read
+    public string PhotoName;
+
+    //total number of photoes 
+    public int maxIndex;
+   
+    //temp index of Page Changer
+    public int tempIndex;
+
+    public Text tempIndexText;
+
+    //audio source play page sound
+    public AudioSource _as;
+
+    public AudioClip[] auClip;
+
+
+
+
+//set the floder path
+    private void Start()
+    {
+
+        PhotoPath = Application.dataPath + "/P";
+
+
+    }
+
+
+//show album,hide buttons by  set scale as zero;
+    public void showPicturePannel() {
+
+
+        if (AlbumState ==false) {
+
+            _as.clip = auClip[1];
+            _as.Play();
+
+            AlbumState = true;
+
+            _AlbumPage.gameObject.SetActive(true);
+
+            foreach (var i in hideBUttons)
+            {
+                i.transform.localScale = Vector3.zero;
+            }
+
+            //call this function ,to show picture ,everytime album opended.
+
+            refreshImage();
+
+
+        }
+        else if (AlbumState == true)
+        {
+
+            AlbumState = false;
+
+            _AlbumPage.gameObject.SetActive(false);
+
+            foreach (var i in hideBUttons)
+            {
+                i.transform.localScale = Vector3.one;
+            }
+
+        }
+
+
+
+    }
+
+    public void refreshImage() {
+
+        PhotoName = LastPhotoName();
+        tempIndexText.text = "最新照片，点击按钮翻页";
+
+        StartCoroutine(GetImage());
+        
+        
+     }
+
+
+    //we need a function ,to get  the lasted one picture in folder
+
+    public string LastPhotoName()
+    {
+
+        string _name;
+
+
+        DirectoryInfo photoDic = new DirectoryInfo(PhotoPath);
+
+
+        FileInfo[] photoFiles = photoDic.GetFiles();
+
+        _name = (photoFiles[photoFiles.Length - 1].Name.Split('.')[0]);
+
+
+        maxIndex = Convert.ToInt32(_name);
+
+        tempIndex = maxIndex;
+        //index++;
+
+        if (maxIndex < 10)
+        {
+
+
+            _name = "/00" + maxIndex.ToString() + ".png";
+
+
+        }
+        else if (maxIndex < 100)
+        {
+            _name = "/0" + maxIndex.ToString() + ".png";
+
+        }
+        else
+        {
+
+            _name = "/" + maxIndex.ToString() + ".png";
+
+
+        }
+
+        
+        return _name;
+
+
+
+    }
+
+
+
+    // load image from ssd .and show on UI
+
+    IEnumerator GetImage()
+
+    {
+
+        WWW www = new WWW(PhotoPath+PhotoName);
+
+        yield return www;
+
+        Debug.Log(PhotoPath + PhotoName);
+
+        //if path right and www download without error
+        if (www != null && string.IsNullOrEmpty(www.error))
+
+        {
+            //same as capture, we need texture2d to save pixel info;
+
+            Texture2D texture = new Texture2D(www.texture.width, www.texture.height);
+
+            texture.SetPixels(www.texture.GetPixels());
+
+            texture.Apply(true);
+
+            texture.filterMode = FilterMode.Trilinear;
+
+            // create a sprite, it can be used as texture of album page
+            Sprite tempSprite = Sprite.Create(texture, new Rect(0, 0, 1920, 1080), new Vector2(0f, 0f));
+
+            //set the sprite
+            _AlbumPage.sprite = tempSprite;
+
+            Debug.Log("www success");
+        }
+        else {
+
+            Debug.Log("www failed");
+            Debug.Log(www.error);
+
+
+        }
+
+    }
+
+
+    // this function for local  pageChanger    function button
+
+    //paraments is 1 or-1 feed in .
+    public void ChangeAlbumButton(int value) {
+
+        _as.clip = auClip[1];
+        _as.Play();
+
+        tempIndex += value;
+
+
+        //tempIndex is for Album Changer, to tell the Refresh Function ,which picture we want to read.
+        string tempPhotoName;
+
+        if (tempIndex>maxIndex)
+        {
+
+            tempIndex = 1;
+        }
+        else if (tempIndex<=0)
+        {
+            tempIndex = maxIndex;
+        }
+
+
+
+        if (tempIndex < 10)
+        {
+
+
+            tempPhotoName = "/00" + tempIndex.ToString() + ".png";
+
+
+        }
+        else if (tempIndex < 100)
+        {
+            tempPhotoName = "/0" + tempIndex.ToString() + ".png";
+
+        }
+        else
+        {
+
+            tempPhotoName = "/" + tempIndex.ToString() + ".png";
+
+
+        }
+
+        //set PhotoName.
+
+        PhotoName = tempPhotoName;
+
+          tempIndexText.text= string.Format("第{0}张，总计{1}张",tempIndex,maxIndex);
+
+     
+        //Change the Photo
+        StartCoroutine(GetImage());
+
+
+    }
+
+
+}
---
+++ photo_shooter.cs
@@ -0,0 +1,205 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+using System;
+
+
+// this behavior capture game window, and save it in local floder. Named by index number autoly..
+public class photo_shooter : MonoBehaviour
+{
+
+    //a simple text to show debug info;
+    public Text _text;
+
+
+    //hide something,usually,UI elements,when capturing.
+    public GameObject[] hideWhenShoot;
+
+    //the path where we save  the photoes
+    private string PhotoPath;
+
+    // name by index number
+    public string PhotoName;
+
+    //shoot sound and count sound
+    public AudioSource _as;
+
+    public AudioClip[] auClip;
+
+    // shoot effect
+    //public Animator _an;
+
+
+    void Start()
+    {
+
+        // full path of picture save Floder. if in editor ,return Asset path.if EXE ,return data floder path;
+        PhotoPath = Application.dataPath + "/P";
+
+        //if the floder does not exist,creat it !
+        if (false == System.IO.Directory.Exists(PhotoPath))
+        {
+
+            System.IO.Directory.CreateDirectory(PhotoPath);
+
+        }
+
+
+        //create the first picture ,and name it 000;
+        StartCoroutine(initiaPhotoDic());
+
+    }
+
+
+    //shoot button clicked ,start count time ,then run the shoot delay function
+    public void shoot()
+    {
+        //hide elements which not needed when capture
+        foreach (var i in hideWhenShoot)
+        {
+            i.transform.localScale = Vector3.zero;
+        }
+
+        //Count 3 animation show, the animator on it will player autoly
+        GameObject.Find("Canvas").transform.GetChild(0).gameObject.SetActive(true);
+
+
+        //play count sound
+        _as.clip = auClip[2];
+        _as.Play();
+
+
+        StartCoroutine(shootDelay());
+
+    }
+
+
+    //we have hide things not needed ,start shoot
+    IEnumerator shootDelay() {
+
+        //wait 3s 
+        yield return new WaitForSeconds(3.0f);
+
+        //shoot sound,shoot effect
+        _as.clip = auClip[0];
+        _as.Play();
+       // _an.Play("n");
+
+        //turn off count3 canvas
+        GameObject.Find("Canvas").transform.GetChild(0).gameObject.SetActive(false);
+
+
+        //progress of picture:1.read to texture2d 2.encode to jpg byte
+        Texture2D Te = new Texture2D(1920, 1080, TextureFormat.RGB24, false);
+        //Texture2D为内存中一种图片纹理格式
+        //位置1,2的两个数字是指定了纹理的宽高。
+        Te.ReadPixels(new Rect(0, 0, 1920, 1080), 0, 0, true);
+        //读取屏幕像素
+        Te.Apply();
+        //保存对Texture2D的更改
+        byte[] bytes = Te.EncodeToJPG();
+        //将Texture2D转换为PNG格式储存在 bytes这个变量中
+
+
+        // to get the name of lastest name of picture
+        PhotoName  = LastPhotoName();
+
+        File.WriteAllBytes(PhotoPath + PhotoName, bytes);
+
+
+       // File.WriteAllBytes(PhotoPath + PhotoName, bytes);
+
+
+        Debug.Log(Application.dataPath);
+
+        
+        //shoot finished, show those elements
+        foreach (var i in hideWhenShoot)
+        {
+            i.transform.localScale = Vector3.one;
+        }
+    }
+
+
+    //basing existed files name , return the newest name 
+    public string LastPhotoName() {
+
+        string _name;
+
+        //new directory class ,feed it photo path
+        DirectoryInfo photoDic = new DirectoryInfo(PhotoPath);
+
+        //get all files in this dic,and save it in a group
+        
+        FileInfo[] photoFiles = photoDic.GetFiles();
+
+        //queued by index name , so we can get the last one's . split it by dot,and return it's number
+        _name = (photoFiles[photoFiles.Length-1].Name.Split('.')[0]);
+
+        //forece convert to int
+        int index = Convert.ToInt32(_name);
+
+        // add one ,because it will be the next name
+        index++;
+
+        //if 6,convert it as"/006.png"
+        if (index < 10)
+        {
+
+
+            _name = "/00" + index.ToString() + ".png";
+
+
+        }
+        else if (index < 100)
+        {
+            _name = "/0" + index.ToString() + ".png";
+
+        }
+        //if 116,convert it as"/116.png"
+        else
+        {
+
+            _name ="/"+index.ToString() + ".png";
+
+
+        }
+
+        //debug the name
+        _text.text = _name;
+        return _name;
+
+        
+
+    }
+
+
+    // call it in start FUnction , create a photo named 000.
+    IEnumerator initiaPhotoDic() {
+
+        yield return null;
+
+        _text.text ="do ienumerator";
+
+
+        Texture2D Tea = new Texture2D(1920, 1080, TextureFormat.RGB24, false);
+
+        Tea.ReadPixels(new Rect(0, 0, 1920, 1080), 0, 0, true);
+
+        Tea.Apply();
+
+        byte[] bytes = Tea.EncodeToJPG();
+
+        File.WriteAllBytes(PhotoPath + "/000.png", bytes);
+
+
+    }
+
+
+}
+
+
+
+
